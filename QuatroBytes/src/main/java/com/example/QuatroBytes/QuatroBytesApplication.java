9af2d169fe10dc59f2a24d71f232996426d1a9fa
michael <michael_douglasfs@hotmail.com>
2026-04-26 15:53:11 -0300

    estrutura inicial do projeto

---
+++ QuatroBytes/src/main/java/com/example/QuatroBytes/QuatroBytesApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuatroBytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuatroBytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuatroBytesApplication.class, args);
+	}
+
+}
---
+++ QuatroBytes/src/test/java/com/example/QuatroBytes/QuatroBytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuatroBytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuatroBytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
