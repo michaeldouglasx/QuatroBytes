90a8eda3bc8fe82ed289af80a2f4ef6ab1eebc00
Michael Douglas Figueirôa dos Santos <06190346103@ibama.gov.br>
2026-06-22 17:02:09 -0300

    Security + UserDetails

---
+++ QuatroBytes/src/main/java/com/example/QuatroBytes/infra/security/AutenticacaoService.java
@@ -0,0 +1,5 @@
+package com.example.QuatroBytes.infra.security;
+
+public class SecurityFilterChainConfig {
+
+}
---
+++ QuatroBytes/src/main/java/com/example/QuatroBytes/infra/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.QuatroBytes.infra.security;
+
+public class SecurityConfig {
+}
